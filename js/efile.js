5bd866634e235a832eca58b60d7a6395a842f45d
Nikunj Prajapati <nikunj231991@gmail.com>
2017-03-18 21:58:57 +0530

    2nd

---
+++ js/efile.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    
+    emailjs.init("user_A5UERdceySkLKO2iou31Q");
+    var myform=$("#contactForm");
+    myform.submit(function (event) {
+        event.preventDefault();
+
+        // Change to your service ID, or keep using the default service
+        var service_id = "outlook";
+        var template_id = "template_cLHc3lBk";
+        var fromname=myform.find("#name").val();
+        var fromemail=myform.find("#email").val();
+        var subject=myform.find("#subject").val();
+        var message=myform.find("#message").val();
+        myform.find("button").html("<i class='fa fa-paper-plane'></i> Sending");
+        var fullbody = "Email : " + fromemail + " Subject : " + subject + " Body : " + message;
+        emailjs.send(service_id,template_id,{reply_to: fromemail, from_name: fromname,to_name:"Nikunj Prajapati",message_html:fullbody})
+          .then(function(){ 
+              alert("Sent!");
+              myform.find("button").text("Send");
+          }, function(err) {
+              alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+              myform.find("button").text("<i class='fa fa-paper-plane'></i> Send");
+          });
+    });
+})();
